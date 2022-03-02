43b02cf41a3ae1368839488aff323921f5cf272f
FanChiMao <qaz5517359@gmail.com>
2022-03-02 21:47:14 +0800

    Add files via upload

---
+++ pretrained_model/download_models.py
@@ -0,0 +1,28 @@
+import wget
+from tqdm import tqdm
+
+def main():
+    with tqdm(total=8) as bar:
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Deblurring_motionblur.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Dehaze_realworld.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Denoise_gaussian.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Denoise_realworld.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Deraining_raindrop.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Deraining_rainstreak.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/LLEnhancement.pth')
+        bar.update(1)
+        wget.download('https://github.com/FanChiMao/SRMNet-thesis/releases/download/v0.0/Retouching.pth')
+        bar.update(1)
+    print('Done !!')
+
+
+
+if __name__ == '__main__':
+    print('Start downloading pretrained models from https://github.com/FanChiMao/SRMNet-thesis/releases/tag/v0.0')
+    main()
